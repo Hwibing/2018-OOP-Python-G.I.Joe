960d44aca01ace1cf0d4e40fb3bd187ea023f481
youngkeun_Jung <birdonmars@sasa.hs.kr>
2018-11-20 01:26:28 +0900

    base class update

--- base_class.py
+++ base_class.py
@@ -2,21 +2,36 @@ if __name__ == '__main__':
     print("HELLO WORLD :: BASE_CLASS MODULE")
 
 
-import random
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def print_price(self):
+        print(self.price)
 
 
-price = [agriculture, livestock,  manufactured, luxury]
+class Expire_Product(Product):
+    def __init__(self, name, price, expire):
+        super().__init__(name, price)
+        self.expire = expire
+        #print(self.name,self.expire,self.price)
 
-
-
-class NewDay:
+class Agriculture_Product(Expire_Product):
     pass
 
 
-class Event:
-    def
+class Livestock_Product(Expire_Product):
     pass
 
 
-class Storage_Event (Event):
+class Luxury_Product(Product):
     pass
+
+
+class Manufactured_Product(Product):
+    pass
+
+a=Agriculture_Product('sample',1200,7)
+a.print_price()
+
