0fbb1258882c1939733b7ba7ed63edd6898bd23b
Hwibing <hjoon0213@naver.com>
2018-11-19 09:48:46 +0900

    add window

--- gui.py
+++ gui.py
@@ -1,3 +1,15 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+
 if __name__=="__main__":
     print("Hello, world!")
     print("This is GUI.")
+
+class window(QWidget):
+    def __init__(self,name):
+        super().__init__()
+        self.name=name
+        self.init_UI(self)
+    def init_UI(self,name):
+        self.setwindowTitle(name)
+        self.show()
