ebbe27e6615d5925335450399ea0503dcfdaf40f
Hwibing <hjoon0213@naver.com>
2018-11-19 10:01:10 +0900

    버그 수정

--- gui.py
+++ gui.py
@@ -5,15 +5,15 @@ if __name__=="__main__":
     print("Hello, world!")
     print("This is GUI.")
 
-class window(QWidget): # QWidget 클래스를 상속함(창)
+class Wind(QWidget):
     def __init__(self,name): # 생성자
-        super().__init__()
-        self.name=name # 창의 이름
-        self.init_UI(self)
-    def init_UI(self,name): # UI 초기화
-        self.setWindowTitle(name) # 창의 이름(init의 name)
+        super().__init__() # 상위 클래스의 생성자 호출
+        self.name=name
+        self.initUI() # initUI 메소드 호출
+    def initUI(self):
+        self.setWindowTitle(self.name) # 창의 제목(위쪽 바에 표시되는...)
         self.show() # 보이기
 
-app=QApplication(sys.argv)
-main_window=window("Millstone")
-sys.exit(app.exec_())
+app=QApplication(sys.argv) # application 객체 생성하기 위해 시스템 인수 넘김
+k=Wind("Main Window")
+sys.exit(app.exec_()) # 이벤트 처리를 위한 루프 실행(메인 루프), 루프가 끝나면 프로그램도 종료
