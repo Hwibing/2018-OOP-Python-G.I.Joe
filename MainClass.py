673dd3c0c2af1d581a0d8d019dd5288db07ddd74
youngkeun_Jung <birdonmars@sasa.hs.kr>
2018-11-23 09:28:22 +0900

    Merge branch 'master' of https://github.com/Hwibing/2018-OOP-Python-Millstone
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- MainClass.py
+++ MainClass.py
@@ -4,9 +4,88 @@ if __name__ == '__main__':
 
 # Define Class
 
-class Settings:
+class GameSettings:
     def __init__(self, money):
         self.money = money
+
+
+class Storage:
+    def __init__(self, maxsize):
+        self.maxsize = maxsize
+        self.quantity = 0
+        self.warehouse = {}
+        self.warehouse_expire = {}
+
+    def overflow(self, add):
+        if self.quantity + add > self.maxsize: return True
+        else: return False
+
+    def buy(self, name, product, number):
+        if not self.overflow(number):
+            self.quantity += number
+            if isinstance(product, ExpireProduct):
+                if name not in self.warehouse_expire:
+                    self.warehouse_expire[name] = [[number, product.productList[name][1]]]
+                else:
+                    if self.warehouse_expire[name][0][1] == product.productList[name][1]:
+                        self.warehouse_expire[name][0][0] += number
+                    else:
+                        self.warehouse_expire[name].append([number, product.productList[name][1]])
+
+            if name not in self.warehouse:
+                self.warehouse[name] = 0
+            self.warehouse[name] += number
+
+        else:
+            print('Error: STORAGE OVERFLOW')
+            return -1
+
+        self.printstorage()
+        return 0
+
+    def sell(self, name, number):
+        if name not in self.warehouse:
+            print('Error: NOT ENOUGH ITEMS')
+            return -1
+        else:
+            if self.warehouse[name] >= number:
+                self.warehouse[name] -= number
+                self.quantity -= number
+                if self.warehouse[name] == 0:
+                    del self.warehouse[name]
+            else:
+                print('Error: NOT ENOUGH ITEMS')
+                return -1
+
+        if name in self.warehouse_expire:
+            for [count, expire] in self.warehouse_expire[name]:
+                self.warehouse_expire[name].remove([count, expire])
+                if count > number:
+                    self.warehouse_expire[name].append([count-number, expire])
+                    break
+                else: number -= count
+        return 0
+
+    def nextday(self, dec=1):
+        for name in list(self.warehouse_expire.keys()):
+            for idx in range(len(self.warehouse_expire)): # size of list
+                self.warehouse_expire[name][idx][1] -= dec
+                if self.warehouse_expire[name][idx][1] <= 0:
+                    self.warehouse_expire[name].remove([self.warehouse_expire[name][idx][0], self.warehouse_expire[name][idx][1]])
+                    self.quantity -= self.warehouse_expire[name][idx][0]
+
+
+
+
+
+    def printstorage(self):
+        print(self.warehouse)
+        print(self.warehouse_expire)
+        print()
+
+
+
+
 
 class Product:
     def __init__(self, type):
@@ -21,10 +100,9 @@ class Product:
 
     def update(self, percent, products):
         for name in products:
-            print(name)
-            print(self.productList[name])
+            #print(name)
+            #print(self.productList[name])
             self.productList[name] = int(self.productList[name]*(percent/100+1))
-
 
 # idx 0 --> price idx 1 --> expire
 class ExpireProduct(Product):
@@ -32,3 +110,6 @@ class ExpireProduct(Product):
     def append(self, name, price, expire):
         self.productList[name] = [price, expire]
 
+    def update(self, percent, products):
+        for name in products:
+            self.productList[name] = [int(self.productList[name][0] * (percent / 100 + 1)), self.productList[name][1]]
