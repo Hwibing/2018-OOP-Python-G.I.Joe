1b579d1fc88a8fea0c772c669f83229ff4e1efe3
Hwibing <hjoon0213@naver.com>
2018-12-16 11:06:11 +0900

    에러 수정, 주석 수정

--- Gui.py
+++ Gui.py
@@ -391,22 +391,24 @@ class Main_wind(Wind):
     def selectionChanged_event(self):
         """
         리스트(물건 목록)에서 선택한 아이템이 바뀌었을 때 호출됩니다.
-        하는 일: 텍스트 바꾸기, 이미지 바꾸기, 현 아이템 바꾸기
+        하는 일: 선택 아이템 이름/가격 받기, 이미지 바꾸기, 텍스트 띄우기
         """
         try:
-            k = str(self.Products.currentItem().text())
-        except AttributeError:
-            return
-        else:
-            if "-" in k or "τ" in k:
+            self.selectedItemText = str(
+                self.Products.currentItem().text())  # 선택된 아이템의 텍스트를 받아온다
+        except AttributeError:  # 그런 거 없다면
+            return  # 리턴
+        else:  # 그런 거 있다면
+            # 만약 물건이 아니라면(안내 텍스트면)
+            if "-" in self.selectedItemText or "τ" in self.selectedItemText:
                 return
-            k = k.split("\t")
+            self.selectedItemText = self.selectedItemText.split("\t")  # 탭으로 분리
 
-        self.current_item_name = str(k[0].strip("\t"))  # 아이템 이름
-        self.current_item_price = int(
-            k[1].strip("\t").replace("\t", ""))  # 아이템 가격
-        self.item_name.setText(self.current_item_name)  # 이름을 띄우고
-        self.item_price.setText(str(self.current_item_price)+" Tau")  # 가격도 띄우고
+        self.current_item_name = str(
+            self.selectedItemText[0].strip("\t"))  # 아이템 이름
+        self.current_item_price=str(self.selectedItemText[1].strip("\t")) # 아이템 가격
+        self.item_name.setText(self.current_item_name) # 아이템 이름 띄우기
+        self.item_price.setText(str(self.current_item_price)) # 아이템 가격 띄우기
 
         # 물건의 클래스 이름을 받아 적절한 이미지 배치
         self.item_class = getclass(self.current_item_name).type
@@ -505,6 +507,7 @@ class Main_wind(Wind):
     def next_day(self):
         """
         다음 날로 넘어갑니다. Day를 증가시키고, storage의 유통기한을 1씩 없앱니다.
+        선택된 아이템의 가격을 변경시켜줍니다.
         파산을 감지합니다. (돈<0)
         Main Window를 제외한 모든 창을 닫고, 새로 뉴스를 띄웁니다.
         """
@@ -519,9 +522,10 @@ class Main_wind(Wind):
         opened_window_list.clear()  # 딕셔너리를 비우고
         opened_window_list[self.name] = self  # 자신을 넣는다
 
+        print(self.selectionChanged_event()) # 안내 메시지 다시 띄우기
         self.refresh()  # 다시 창 띄우기
         if money.money < 0:  # 돈이 0보다 적으면
-            QMessageBox().about(self, "Bankrupt", "You are bankrupt!")  # 파산 알림
+            alert_message(self, "Bankrupt", "You are bankrupt!")
             self.restart_button = Moveto_button(
                 "Restart", "Restart game.", self, Intro_wind, "Restart")  # 보이지 않는 버튼
             self.restart_button.click()  # 게임 재시작
@@ -536,6 +540,7 @@ class Main_wind(Wind):
         self.Info_text.setText("{}번째 주\n남은 돈: {}\n창고 용량: {}/{}".format(
             Day, money.money, storage.quantity, storage.maxsize))  # 텍스트 재설정
         self.showProducts()  # 사진 다시 띄우기, 리스트 다시 출력.
+
         super().refresh()  # 상위 클래스의 refresh 불러옴
 
 
